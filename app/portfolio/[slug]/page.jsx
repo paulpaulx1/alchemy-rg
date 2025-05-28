f0f0a484911e718a018aa3bc0e422e20e2f5123f
maxx-css <hellomauriciorivera@gmail.com>
2025-05-28 19:25:20 -0400

    Fetched artwork order changed to descending

--- app/portfolio/[slug]/page.jsx
+++ app/portfolio/[slug]/page.jsx
@@ -33,7 +33,7 @@ export default async function Portfolio({ params }) {
       title,
       "slug": slug.current
     },
-    "artworks": *[_type == "artwork" && portfolio._ref == ^._id] | order(order asc) {
+    "artworks": *[_type == "artwork" && portfolio._ref == ^._id] | order(order desc) {
       _id,
       title,
       displayTitle,
