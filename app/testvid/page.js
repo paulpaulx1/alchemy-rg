09fd36dbf61fa77f6baa15d5b054cd1e763f0dff
Paul Neenan <paulneenan@Mac.lan>
2025-08-12 15:29:32 -0400

    apo

--- app/testvid/page.js
+++ app/testvid/page.js
@@ -1,3 +1,4 @@
+
 // pages/youtube-test.js (or app/youtube-test/page.js for App Router)
 
 export default function YouTubeTest() {
@@ -32,8 +33,8 @@ export default function YouTubeTest() {
             width: '100%',
             height: '100%'
           }}
-          src="https://www.youtube.com/embed/34glwxP8phM"
-          title="YouTube video player"
+          src="https://www.youtube.com/embed/34glwxP8phM?playsinline=1&rel=0&modestbranding=1&controls=1"
+         title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
